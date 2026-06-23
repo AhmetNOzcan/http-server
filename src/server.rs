0310fcfb137e6293773f3a950a1620a8926b9a82
Ahmet Ozcan <ahmet.ozcan@oyakyatirim.com.tr>
2026-06-23 21:15:33 +0300

    server

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::{io::Read, net::TcpListener};
 pub struct Server {
     addr: String,
 }
@@ -9,5 +10,26 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        loop {
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    println!("Connection established with {}", addr);
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            let request = String::from_utf8_lossy(&buffer);
+                            println!("Request: {}", request);
+                        }
+                        Err(e) => {
+                            eprintln!("Failed to read from connection: {}", e);
+                        }   
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Failed to accept connection: {}", e);
+                }
+            }            
+        }
     }
 }
